e60c4f2b221c9be0e9334b5567653afc9bff9b75
Eetu Huotari <eetuh25@gmail.com>
2024-04-04 03:58:16 +0300

    working card animation

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="bg-split-half h-viewport font-raleway">
+  <div
+    ref="mainPageRef"
+    class="main-page bg-split-half h-viewport font-raleway overflow-x-hidden"
+  >
     <div class="mx-[80px] pt-20">
-
       <!-- Lang button holder -->
       <div class="w-full flex justify-end items-center cursor-pointer">
         <div
@@ -84,8 +86,10 @@
           <img
             :src="logo.src"
             :alt="logo.name"
-            :class="{ 'z-30': hoveredIndex === index }"
-            class="pointer-events-none w-40 h-40 z-10"
+            :class="{
+              'z-30 scale-[1.02]': hoveredIndex === index,
+            }"
+            class="pointer-events-none z-10"
           />
           <div
             :class="[
@@ -93,11 +97,13 @@
               logoColorClasses[index],
               { 'hover:z-20': hoveredIndex === index },
             ]"
-            class="absolute h-full rounded-icon-computer w-full top-0 hover:w-[225%] hover:h-[165%]"
+            class="absolute h-full rounded-icon-computer w-full top-0 hover:w-[223.5%] hover:h-[165%]"
           >
             <div
-              :class="{ 'z-30 opacity-100': hoveredIndex === index }"
-              class="relative h-full flex w-full justify-center opacity-0 z-10 pointer-events-none"
+              :class="{
+                'z-30 duration-1000 opacity-100': hoveredIndex === index,
+              }"
+              class="relative h-full flex w-full justify-center opacity-0 z-10 pointer-events-none transition-opacity"
             >
               <div
                 v-if="
@@ -121,8 +127,10 @@
             </div>
           </div>
           <div
-            :class="{ 'z-30 opacity-100': hoveredIndex === index }"
-            class="opacity-0 z-10 pointer-events-none"
+            :class="{
+              'z-30 duration-1000 opacity-100': hoveredIndex === index,
+            }"
+            class="opacity-0 z-10 pointer-events-none transition-opacity"
           >
             <h1 class="text-4xl">{{ $t(`${logo.name}.title`) }}</h1>
           </div>
@@ -131,16 +139,18 @@
 
       <!-- Projects section -->
       <div class="flex items-center justify-center">
-                <h1 class="text-6xl">{{ $t("projects") }}</h1>
+        <h1 class="text-6xl">{{ $t("projects") }}</h1>
       </div>
-      <div class="flex items-center">
-        <div @click="prevSlide()" class="cursor-pointer mr-3">
+      <div class="flex items-center relative">
+        <!-- SwitchButton -->
+        <div @click="prevSlide()" class="cursor-pointer mr-3 animate-bounce z-10">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="35"
             height="40"
             viewBox="0 0 35 40"
             fill="none"
+            class="hover:scale-110 transition-scale duration-300"
           >
             <path
               d="M3.5 26.0622C-1.16667 23.3679 -1.16667 16.6321 3.5 13.9378L24.5 1.81347C29.1667 -0.880836 35 2.48704 35 7.87564L35 32.1244C35 37.513 29.1667 40.8808 24.5 38.1865L3.5 26.0622Z"
@@ -148,8 +158,12 @@
             />
           </svg>
         </div>
+        <!-- Card -->
+        <div class="w-full relative">
         <div
-          class="w-full h-[800px] bg-split-half-reverse my-14 rounded-icon-computer flex"
+          class="w-full h-[800px] bg-split-half-reverse my-14 rounded-icon-computer flex z-10"
+          :style="realSlideStyle"
+          @transitionend="resetTransition"
         >
           <div class="p-12 w-1/2">
             <h1 class="mb-8">
@@ -162,19 +176,45 @@
             </p>
           </div>
           <div class="w-1/2 px-14 pt-32">
-            <img class="pb-2" src="@/assets/Picture3.jpg" />
+            <img class="pb-4" src="@/assets/Picture3.jpg" />
             <div>
-              <p class="text-4xl">{{ $t("toolsused") }}</p>
+              <p class="text-4xl text-white">{{ $t("toolsused") }}</p>
             </div>
           </div>
         </div>
-        <div @click="nextSlide()" class="cursor-pointer ml-3">
+        <!-- Animation Card -->
+        <div
+          class="h-[800px] w-full bg-split-half-reverse my-14 rounded-icon-computer absolute inset-0 flex z-0"
+          @transitionend="resetTransition"
+          ref="fakeCardElement"
+        >
+          <div class="p-12 w-1/2 flex flex-col">
+            <h1 class="mb-8">
+              <span class="text-5xl">{{
+                $t(`${projectData[SlideIndex].translationKey}.name`)
+              }}</span>
+            </h1>
+            <p class="text-4xl">
+              {{ $t(`${projectData[SlideIndex].translationKey}.description`) }}
+            </p>
+          </div>
+          <div class="w-1/2 px-14 pt-32 flex flex-col justify-start">
+            <img class="pb-4" src="@/assets/Picture3.jpg" />
+            <div>
+              <p class="text-4xl text-white">{{ $t("toolsused") }}</p>
+            </div>
+          </div>
+        </div>
+        </div>
+        <!-- SwitchButton -->
+        <div @click="nextSlide()" class="cursor-pointer ml-3 animate-bounce z-10">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="35"
             height="40"
             viewBox="0 0 35 40"
             fill="none"
+            class="hover:scale-110 transition-scale duration-300"
           >
             <path
               d="M31.5 13.9378C36.1667 16.6321 36.1667 23.3679 31.5 26.0622L10.5 38.1865C5.83333 40.8808 0 37.513 0 32.1244V7.87564C0 2.48704 5.83333 -0.880835 10.5 1.81347L31.5 13.9378Z"
@@ -185,7 +225,7 @@
       </div>
 
       <!-- Contact section -->
-      <div class="w-full h-[800px] mt-44 pb-24 flex">
+      <div class="w-full h-[900px] mt-44 pb-36 flex">
         <div class="w-1/2 text-white space-y-14">
           <h1 class="text-5xl">{{ $t("contact") }}</h1>
           <div class="flex items-center">
@@ -196,7 +236,12 @@
             />
             <div class="flex flex-col">
               <p class="text-4xl">{{ $t("call") }}</p>
-              <p class="text-4xl">+358 44074 2509</p>
+              <a
+                :href="`tel:+358440742509`"
+                class="text-4xl underline underline-offset-8 duration-100 transition:font hover:font-medium"
+              >
+                +358 44074 2509
+              </a>
             </div>
           </div>
           <div class="flex items-center">
@@ -207,57 +252,67 @@
             />
             <div class="flex flex-col">
               <p class="text-4xl">{{ $t("sendemail") }}</p>
-              <p class="text-4xl">contact@eetuhuotari.dev</p>
+              <a
+                href="mailto:contact@eetuhuotari.dev"
+                class="text-4xl underline underline-offset-8 decoration-white duration-100 transition:font hover:font-medium"
+              >
+                contact@eetuhuotari.dev
+              </a>
             </div>
           </div>
           <div>
             <p class="text-4xl mb-4">{{ $t("socials") }}</p>
             <div class="flex gap-6">
               <div
-                class="w-20 h-20 bg-white rounded-full flex items-center justify-center"
+                class="w-20 h-20 bg-white rounded-full flex items-center justify-center hover:scale-110 transition-scale duration-300"
               >
                 <a href="https://github.com/Eetuh123">
                   <img
                     src="@/assets/icons/github.svg"
                     alt="github"
-                    class="w-16 h-16"
+                    class="w-16 h-16 hover:scale-110 transition-scale duration-300"
                   />
                 </a>
               </div>
               <div
-                class="w-20 h-20 bg-white rounded-full flex items-center justify-center"
+                class="w-20 h-20 bg-white rounded-full flex items-center justify-center hover:scale-110 transition-scale duration-300"
               >
                 <a href="https://www.linkedin.com/in/eetu-huotari-594106238/">
                   <img
                     src="@/assets/icons/linkedin.svg"
                     alt="linkedin"
-                    class="w-16 h-16"
+                    class="w-16 h-16 hover:scale-110 transition-scale duration-300"
                   />
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <div class="w-1/2 flex">
+
+        <!-- Email form -->
+        <div class="w-1/2 flex flex-col">
           <div class="w-full pl-12">
+            <div class="pl-7 pb-12 text-5xl">
+              <h1>{{ $t("form.formtitle") }}</h1>
+            </div>
             <form class="flex flex-col" @submit.prevent="sendEmail">
               <input
                 v-model="form.email"
-                class="bg-dark text-white rounded-icon-computer p-10 mb-12"
+                class="bg-dark text-white rounded-icon-computer p-10 mb-10 font-semibold text-lg"
                 type="email"
                 name="email"
                 :placeholder="$t('form.email')"
               />
               <input
                 v-model="form.name"
-                class="bg-dark text-white rounded-icon-computer p-10 mb-10"
+                class="bg-dark text-white rounded-icon-computer p-10 mb-10 font-semibold text-lg"
                 type="text"
                 name="name"
                 :placeholder="$t('form.name')"
               />
               <input
                 v-model="form.subject"
-                class="bg-dark text-white rounded-icon-computer p-10 mb-12"
+                class="bg-dark text-white rounded-icon-computer p-10 mb-10 font-semibold text-lg"
                 type="text"
                 name="subject"
                 :placeholder="$t('form.subject')"
@@ -265,14 +320,14 @@
               <div class="flex">
                 <textarea
                   v-model="form.message"
-                  class="bg-dark text-white rounded-icon-computer pl-10 p-6 mr-12 flex-1"
+                  class="bg-dark text-white rounded-icon-computer pl-10 p-6 mr-12 flex-1 font-semibold text-lg"
                   name="message"
                   :placeholder="$t('form.message')"
                 ></textarea>
                 <input
                   type="submit"
                   :value="$t('form.send')"
-                  class="flex-initial bg-dark text-white rounded-icon-computer px-10 py-20 c"
+                  class="flex-initial bg-dark text-white rounded-icon-computer px-10 py-20 font-bold cursor-pointer"
                   style="width: auto"
                 />
               </div>
@@ -280,13 +335,12 @@
           </div>
         </div>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, computed} from "vue";
 import { logosData } from "@/assets/logosData.js";
 import { projectData } from "@/assets/projectData.js";
 import emailjs from "@emailjs/browser";
@@ -301,12 +355,20 @@ export default {
     const logoColorClasses = ref([]);
     const logoAnimationClasses = ref([]);
     const SlideIndex = ref(0);
+    const transitioning = ref(false);
+    const direction = ref("next");
     const hoveredIndex = ref(null);
+    const fakeCardElement = ref(null);
     const form = ref({
       email: "",
       name: "",
       subject: "",
       message: "",
+    });
+
+    onMounted(() => {
+      window.addEventListener("resize", updateLogoClasses);
+      updateLogoClasses();
     });
 
     function toggleLanguage() {
@@ -323,7 +385,9 @@ export default {
         const rect = logoRef.getBoundingClientRect();
         const midpoint = window.innerWidth / 2;
         logoColorClasses.value[index] =
-          rect.left + rect.width / 2 < midpoint ? "bg-white" : "bg-dark";
+          rect.left + rect.width / 2 < midpoint
+            ? "bg-white text-dark"
+            : "bg-dark text-white";
       });
     };
 
@@ -373,20 +437,56 @@ export default {
       hoveredIndex.value = null;
     };
 
-    const nextSlide = () => {
-      if (SlideIndex.value < projectData.length - 1) {
-        SlideIndex.value++;
+    const getNextSlideIndex = (current, direction, length) => {
+      if (direction === "next") {
+        return (current + 1) % length;
       } else {
-        SlideIndex.value = 0;
+        return current - 1 < 0 ? length - 1 : current - 1;
       }
     };
 
+    const nextSlide = () => {
+      if (transitioning.value) return;
+      direction.value = "next";
+      fakeCardElement.value.style.transition = 'none';
+      fakeCardElement.value.style.transform = 'translateX(-120%)';
+      requestAnimationFrame(() => {
+        fakeCardElement.value.style.transition = 'transform 2s ease';
+        fakeCardElement.value.style.transform = 'translateX(0%)';
+      });
+      transitioning.value = true;
+    };
+
     const prevSlide = () => {
-      if (SlideIndex.value > 0) {
-        SlideIndex.value--;
-      } else {
-        SlideIndex.value = projectData.length - 1;
-      }
+      if (transitioning.value) return;
+      direction.value = "prev";
+      fakeCardElement.value.style.transition = 'none';
+      fakeCardElement.value.style.transform = 'translateX(120%)';
+      requestAnimationFrame(() => {
+        fakeCardElement.value.style.transition = 'transform 2s ease';
+        fakeCardElement.value.style.transform = 'translateX(0%)';
+      });
+      transitioning.value = true;
+    };
+
+    const realSlideStyle = computed(() => {
+      if (!transitioning.value) return {};
+      return {
+        transform:
+          direction.value === "next" ? "translateX(120%)" : "translateX(-120%)",
+        transition: "transform 2s ease",
+      };
+    });
+
+
+    
+    const resetTransition = () => {
+      SlideIndex.value = getNextSlideIndex(
+        SlideIndex.value,
+        direction.value,
+        projectData.length
+      );
+      transitioning.value = false;
     };
 
     const sendEmail = async () => {
@@ -419,11 +519,6 @@ export default {
       calculateAnimationClasses();
     };
 
-    onMounted(() => {
-      window.addEventListener("resize", updateLogoClasses);
-      updateLogoClasses();
-    });
-
     onUnmounted(() => {
       window.removeEventListener("resize", updateLogoClasses);
     });
@@ -440,10 +535,13 @@ export default {
       handleMouseEnter,
       handleMouseLeave,
       hoveredIndex,
+      SlideIndex,
       nextSlide,
       prevSlide,
+      realSlideStyle,
+      resetTransition,
       projectData,
-      SlideIndex,
+      fakeCardElement,
     };
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
         fontFamily: {
           'raleway': ['Raleway', 'sans-serif'],
         },
+        boxShadow: {
+          'icon-shadow': '0px 0px 4px rgba(0, 0, 0, 0.01)',
+        },
         colors: {
             'white': '#ffffff',
             'dark': '#232121',
@@ -29,10 +32,29 @@ module.exports = {
                 '0%': { transform: 'scale(0.45)', transformOrigin: 'top right'},
                 '100%': { transform: 'scale(1)', transformOrigin: 'top right'},
               },
-        },
+              bounce: {
+                '0%, 100%': { transform: 'translateY(-2%)', 'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)' },
+                '50%': { transform: 'translateY(2%)', 'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)' },
+              },
+              buttonPress: {
+                '0%': { transform: 'scale(1)'},
+                '100%': { transform: 'scale(1.1)'},
+              },
+              left: {
+                '0%': { transform: 'translateX(-120%)'},
+                '100%': { transform: 'translateX(0%)'},
+              },
+              right: {
+                '0%': { transform: 'translateX(120%)'},
+                '100%': { transform: 'translateX(0%)'},
+              },
+            },
         animation: {
-            'grow': 'grow linear linear',
+            'grow': 'grow linear',
             'growreverse': 'growReverse linear',
+            'bounce': 'bounce 3s infinite linear',
+            'right': 'right 1s forwards',
+            'left': 'left 1s forwards',
 
         },
       },
